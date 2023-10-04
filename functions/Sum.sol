4f163c82108d989a5ea3df0d1e59c2e521b23b6c
Isai <47548422+isaiisj@users.noreply.github.com>
2023-10-04 13:02:39 -0600

    Update Sum.sol

--- functions/Sum.sol
+++ functions/Sum.sol
@@ -8,7 +8,7 @@ contract Sum {
     function sum(int256 a, int256 b) public pure returns (int256 result) {
         result = a + b;
         /*Si intentamos sumar number a result generara un error ya que
-		* no lee ni modificamos variables de estado.
-		*/
+	* no lee ni modificamos variables de estado.
+	*/
     }
 }
