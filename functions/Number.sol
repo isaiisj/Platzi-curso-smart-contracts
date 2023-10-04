e9653c85eb97ebd8ddbfa34fba6c19f93f47442d
Isai <47548422+isaiisj@users.noreply.github.com>
2023-10-04 12:25:39 -0600

    Update Number.sol

--- functions/Number.sol
+++ functions/Number.sol
@@ -3,6 +3,7 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Number {
+    //Si no ponemos pure, obtenemos un warning debido a que solo obtenemos un dato.
     function getNumber() private pure returns (int256 number) {
         number = 1948;
     }
