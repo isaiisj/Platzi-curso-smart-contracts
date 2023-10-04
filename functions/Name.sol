a59bf88b272f0bc441d6ae649d62bf53697d2198
Isai <47548422+isaiisj@users.noreply.github.com>
2023-10-04 12:57:18 -0600

    Update Name.sol

--- functions/Name.sol
+++ functions/Name.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Asset {
     string name = "PlatziCoin";
-
+    //name = "Platzi-Coin; Hacer esto genera un error ya que solo es lectura.
     function getName() public view returns (string memory) {
         return name;
     }
