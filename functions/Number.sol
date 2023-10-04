5a18f66c9ad5104cd73f09bedad33249184409c3
Isai <47548422+isaiisj@users.noreply.github.com>
2023-10-04 12:28:04 -0600

    Update Number.sol

--- functions/Number.sol
+++ functions/Number.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Number {
     //Función privada que no recibe parámetros y regresa un entero.
-	//Si la hacemos pública podemos obtener el numero.
+    //Si la hacemos pública podemos obtener el numero.
     //Si no ponemos pure, obtenemos un warning debido a que solo obtenemos un dato.
     function getNumber() private pure returns (int256 number) {
         number = 1948;
